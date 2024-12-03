1ee2975d837736c765c2db586a313f25f8b3cbbf
mli254 <mli254@ucsc.edu>
2024-12-02 19:50:16 -0800

    added timer and removed print statement

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using TMPro;
+using System;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TextMeshProUGUI timerText;
+    private float timerValue = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
+        countUpTimer();
+    }
+
+    private void countUpTimer() {
+        timerValue += Time.deltaTime;
+        timerText.text = "Timer: " + Convert.ToInt32(timerValue);
     }
 }
